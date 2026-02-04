670892f04fa1185b35429d044a278882c2e9bd21
Igor <ig.chavesbr@gmail.com>
2026-02-05 02:47:03 +0900

    create endpoint register

--- server/server.js
+++ server/server.js
@@ -6,4 +6,8 @@ app.use(cors())
 app.use(express.json())
 
 app.get("/", (req, res) => res.send("PAGINA INICIAL"))
+app.post("/register", (req, res) => {
+  const { user, password } = req.body
+  console.log(user, password)
+})
 app.listen(1000, () => console.log("servidor iniciado"))
