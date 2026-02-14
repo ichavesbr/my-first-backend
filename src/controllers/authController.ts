e73a036c96e33e2f3d2a9caf39b2b0a1a85a6465
Igor <ig.chavesbr@gmail.com>
2026-02-15 03:01:05 +0900

    fix authentication

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,9 +1,31 @@
+import type { Request, Response } from "express"
+import bcrypt from "bcrypt"
 import jwt from "jsonwebtoken"
+import { getUserByEmail } from "../models/userModel.js"
 
 const maxAge = 3 * 24 * 60 * 60 // valid for 3 days
 
-const createToken = (id: number) => {
-  return jwt.sign({ id }, "secret temporary", { expiresIn: maxAge })
+const createToken = (id: number) => jwt.sign({ id }, "secret temporary", { expiresIn: maxAge })
+
+const internalServerErrorMsg = (res: Response, error: any) => {
+  console.error(error)
+  res.status(500).json({ message: "Internal server error" })
 }
 
-export { createToken }
+const loginHandler = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body
+    if (!email || !password) return res.send("❌❌❌❌   please insert email AND password    ❌❌❌❌")
+
+    const user = await getUserByEmail(email)
+    if (!user) return res.send("❌❌❌❌   EMAIL not registered    ❌❌❌❌")
+
+    const auth = await bcrypt.compare(password, user.password)
+    if (!auth) return res.send("❌❌❌❌   PASSWORD is wrong    ❌❌❌❌")
+
+    res.send("✅✅✅✅   user logged, welcome   ✅✅✅✅")
+  } catch (error: any) {
+    internalServerErrorMsg(res, error)
+  }
+}
+export { loginHandler, createToken }
--- src/routes/loginRoute.ts
+++ src/routes/loginRoute.ts
@@ -1,27 +1,9 @@
 import { Router } from "express"
-import bcrypt from "bcrypt"
-import { getUserByEmail } from "../models/userModel.js"
+import { loginHandler } from "../controllers/authController.js"
 
 const loginRoute = Router()
 
-loginRoute.get("/", (req, res) => res.send("Faca seu login"))
-
-loginRoute.post("/", async (req, res) => {
-  const { email, password } = req.body
-  const user = await getUserByEmail(email)
-  const auth = bcrypt.compare(password, user?.password)
-  // TA DANDO ERRO AQUI TA ACEITANDO LOGIN MESMO COM SENHA ERRADA
-  // com email errado da erro como esperado
-
-  if (user) {
-    console.log("user vindo do login post", user)
-    if (auth) {
-      res.send("usuário logado com sucesso")
-      return user
-    }
-    throw Error("incorrect password")
-  }
-  throw Error("incorrect email")
-})
+loginRoute.get("/", (req, res) => res.send("Log in with your account"))
+loginRoute.post("/", loginHandler)
 
 export { loginRoute }
