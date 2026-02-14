794293441bfac3cdc1b97cc4d9cb9a1ad7445741
Igor <ig.chavesbr@gmail.com>
2026-02-15 03:40:38 +0900

    refactor authController, remove unnecessary lines

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,16 +1,9 @@
-import type { Request, Response } from "express"
 import bcrypt from "bcrypt"
 import jwt from "jsonwebtoken"
+import type { Request, Response } from "express"
 import { getUserByEmail } from "../models/userModel.js"
 
 const maxAge = 3 * 24 * 60 * 60 // valid for 3 days
-
-const createToken = (id: number) => jwt.sign({ id }, "secret temporary", { expiresIn: maxAge })
-
-const internalServerErrorMsg = (res: Response, error: any) => {
-  console.error(error)
-  res.status(500).json({ message: "Internal server error" })
-}
 
 const loginHandler = async (req: Request, res: Response) => {
   try {
@@ -33,4 +26,4 @@ const loginHandler = async (req: Request, res: Response) => {
   }
 }
 
-export { loginHandler, createToken }
+export { loginHandler }
